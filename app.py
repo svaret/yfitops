8cab51068c161a2b9e235027036af558ac83a840
Pastorn <lasse.e.bergstrom@gmail.com>
2022-01-08 17:29:09 +0100

    Now fetching from discogs what is is sent in as query parameter 'artist'

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from random import randrange
+
 import discogs_client
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,10 +16,23 @@ def index():
     return render_template('index.html', uri=albums[1].images[1]['uri'])
 
 
+@app.route('/artist')
+def artist():
+    response = requests.get(
+        'https://api.discogs.com/database/search?artist=' + request.args.get('name') + '&type=master&format=LP',
+        headers={'Authorization': 'Discogs token=AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'}
+    )
+    ix = randrange(len(response.json()['results']))
+    return render_template('index.html',
+                           uri=response.json()['results'][ix]['cover_image'])
+
+
 @app.route('/curl')
 def curl():
+    print(request.args.get('user'))
     response = requests.get(
         'https://api.discogs.com/releases/3721310',
         headers={'Authorization': 'Discogs token=AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'}
     )
-    return render_template('index.html', uri=[image["uri"] for image in response.json()['images'] if image["type"] == "primary"][0])
+    return render_template('index.html',
+                           uri=[image["uri"] for image in response.json()['images'] if image["type"] == "primary"][0])
