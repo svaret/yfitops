1b2a3f45ce0e0ee274fc37be50cd756389102bb5
Pastorn <lasse.e.bergstrom@gmail.com>
2022-01-08 13:36:27 +0100

    Only displaying one cover art

--- app.py
+++ app.py
@@ -10,13 +10,8 @@ def index():
     d = discogs_client.Client('ExampleApplication/0.1',
                               user_token="AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD")
     results = d.search(artist='Heptones', type='master', format='LP')
-    first = results.page(1)[0]
-    image = first.images[0]
     albums = results.page(1)
-    uris = []
-    for a in albums:
-        uris.append(a.images[0]['uri'])
-    return render_template('index.html', posts=uris)
+    return render_template('index.html', uri=albums[0].images[0]['uri'])
 
 
 @app.route('/curl')
