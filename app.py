653a9146fde7e01c02c8ca14f8f48d4076b88f14
Pastorn <lasse.e.bergstrom@gmail.com>
2022-01-08 13:28:53 +0100

    Added test of curl call instead of via discogs_client

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import discogs_client
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  d = discogs_client.Client('ExampleApplication/0.1',
-                            user_token="AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD")
-  results = d.search(artist='Heptones', type='master', format='LP')
-  first = results.page(1)[0]
-  image = first.images[0]
-  albums = results.page(1)
-  uris = []
-  for a in albums:
-    uris.append(a.images[0]['uri'])
-  return render_template('index.html', posts=uris)
+    d = discogs_client.Client('ExampleApplication/0.1',
+                              user_token="AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD")
+    results = d.search(artist='Heptones', type='master', format='LP')
+    first = results.page(1)[0]
+    image = first.images[0]
+    albums = results.page(1)
+    uris = []
+    for a in albums:
+        uris.append(a.images[0]['uri'])
+    return render_template('index.html', posts=uris)
+
+
+@app.route('/curl')
+def curl():
+    response = requests.get(
+        'https://api.discogs.com/database/search?artist=Heptones&format=LP&type=master',
+        headers={'Authorization: Discogs token=AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'}
+    )
+    print('ajajajaja')
+    return "Hej"
