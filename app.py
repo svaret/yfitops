e4447c62a72505bfca4abf6252f5e54dd54889a3
Pastorn <lasse.e.bergstrom@gmail.com>
2022-12-23 18:14:09 +0100

    Extracting Discogs token as constant

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import discogs_client
 import requests
 from flask import Flask, render_template, request
 
+DISCOGS_TOKEN = 'AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'
+
 app = Flask(__name__)
 
 
 @app.route('/discogs-client')
 def index():
     d = discogs_client.Client('ExampleApplication/0.1',
-                              user_token="AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD")
+                              user_token=DISCOGS_TOKEN)
     results = d.search(artist='Heptones', type='master', format='LP')
     albums = results.page(1)
     return render_template('index.html', uri=albums[1].images[1]['uri'])
@@ -21,7 +23,7 @@ def artist():
     artist = request.args.get('artist')
     response = requests.get(
         'https://api.discogs.com/database/search?artist=' + artist + '&type=master&format=LP',
-        headers={'Authorization': 'Discogs token=AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'}
+        headers={'Authorization': ('Discogs token=%s' % DISCOGS_TOKEN)}
     )
     number_of_hits = len(response.json()['results'])
     if number_of_hits == 0:
@@ -35,7 +37,7 @@ def curl():
     print(request.args.get('user'))
     response = requests.get(
         'https://api.discogs.com/releases/3721310',
-        headers={'Authorization': 'Discogs token=AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'}
+        headers={'Authorization': ('Discogs token=%s' % DISCOGS_TOKEN)}
     )
     return render_template('index.html',
                            uri=[image["uri"] for image in response.json()['images'] if image["type"] == "primary"][0])
