bad8223feab3eec86d6f1dc3dd770fa307804ef9
Pastorn <lasse.e.bergstrom@gmail.com>
2023-01-03 13:16:19 +0100

    Formatting

--- app.py
+++ app.py
@@ -42,29 +42,31 @@ def artist():
     image_uri = random_entry['cover_image']
     title = random_entry['title']
     VIDEOID = getYoutubeid(title)
-    return render_template('index.html', artist=artist, uri=image_uri, title=title, title_length=len(title), videolink=VIDEOID )
+    return render_template('index.html', artist=artist, uri=image_uri, title=title, title_length=len(title),
+                           videolink=VIDEOID)
 
 
 def getYoutubeid(title):
     YOUTUBE_API_SERVICE_NAME = "youtube"
     YOUTUBE_API_VERSION = "v3"
     videos = []
-    videoid =""
+    videoid = ""
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=GOOGLE_API_KEY)
     try:
-    ### Hämta ut den 1:a träffen på youtube = oftast bästa träffen för aktuell skiva
+        ### Hämta ut den 1:a träffen på youtube = oftast bästa träffen för aktuell skiva
         search_response = youtube.search().list(q=title, part="id,snippet", maxResults=1).execute()
         for search_result in search_response.get("items", []):
             if search_result["id"]["kind"] == "youtube#video":
                 videoid += ("%s" % (search_result["id"]["videoId"]))
-        print (videoid)
-    ### Ifall man vill ha flera länkar sätt maxResults till mer än 1 ovan och gör något av svaret:
+        print(videoid)
+        ### Ifall man vill ha flera länkar sätt maxResults till mer än 1 ovan och gör något av svaret:
         for search_result in search_response.get("items", []):
             if search_result["id"]["kind"] == "youtube#video":
-               videos.append("%s (%s)" % (search_result["snippet"]["title"],                                       search_result["id"]["videoId"]))
+                videos.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["videoId"]))
         for video in videos:
-            print (video)
-    except Exception as e: print(e)
+            print(video)
+    except Exception as e:
+        print(e)
     return videoid
 
 
