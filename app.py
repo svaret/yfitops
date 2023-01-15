d7ed08e70c1682f3cd59e5ca0d6ec05f8496df4c
Supergillie <peter.krooks@gmail.com>
2023-01-15 13:35:15 +0100

    Test av att spela upp skivan man tittar på (#30)
    Button that enables playing of youtube video
    
    Co-authored-by: Peter Krooks <pekr@Peters-MacBook-Pro.local>
    Co-authored-by: Pastorn <lasse.e.bergstrom@gmail.com>

--- app.py
+++ app.py
@@ -1,46 +1,57 @@
 from random import randrange
 
-import discogs_client
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+import sys
+import os
+import urllib
+
 import requests
 from flask import Flask, render_template, request
 
 DISCOGS_TOKEN = 'AugrlbeikovAiGkBIqufmThyfiuRkyNboopdSFWD'
+GOOGLE_API_KEY = 'AIzaSyBoaF8Iw2iP617qopJSC1N1QtDJiq4_Wk8'
+
+VIDEOID = ""
+IMAGE_URI = ""
+TITLE = ""
+ARTIST = ""
 
 app = Flask(__name__)
 
 
-@app.route('/discogs-client')
-def index():
-    d = discogs_client.Client('ExampleApplication/0.1',
-                              user_token=DISCOGS_TOKEN)
-    results = d.search(artist='Heptones', type='master', format='LP')
-    albums = results.page(1)
-    return render_template('index.html', uri=albums[1].images[1]['uri'])
-
-
-@app.route('/')
-def artist():
+@app.route('/youtube')
+def youtube():
     artist = request.args.get('artist')
+    image_uri = request.args.get('imageUri')
+    title = request.args.get('title')
+    return render_template('index.html', artist=artist, uri=image_uri, title=title, videolink=youtubeid(title))
+
+
+@app.route('/album-cover')
+def album_cover():
+    ARTIST = request.args.get('artist')
     response = requests.get(
-        'https://api.discogs.com/database/search?artist=' + artist + '&type=master&format=LP',
+        'https://api.discogs.com/database/search?artist=' + ARTIST + '&type=master&format=LP',
         headers={'Authorization': ('Discogs token=%s' % DISCOGS_TOKEN)}
     )
     number_of_hits = len(response.json()['results'])
     if number_of_hits == 0:
         return render_template('artist-not-found.html', artist=artist)
     random_entry = response.json()['results'][randrange(number_of_hits)]
-    image_uri = random_entry['cover_image']
-    title = random_entry['title']
-    print(random_entry)
-    return render_template('index.html', artist=artist, uri=image_uri, title=title, title_length=len(title))
+    IMAGE_URI = random_entry['cover_image']
+    TITLE = random_entry['title']
+    return render_template('index.html', artist=ARTIST, uri=IMAGE_URI, title=TITLE)
 
 
-@app.route('/curl')
-def curl():
-    print(request.args.get('user'))
-    response = requests.get(
-        'https://api.discogs.com/releases/3721310',
-        headers={'Authorization': ('Discogs token=%s' % DISCOGS_TOKEN)}
-    )
-    return render_template('index.html',
-                           uri=[image["uri"] for image in response.json()['images'] if image["type"] == "primary"][0])
+def youtubeid(title):
+    youtube = build("youtube", "v3", developerKey=GOOGLE_API_KEY)
+    try:
+        search_response = youtube.search().list(q=title, part="id,snippet", maxResults=1).execute()
+        for search_result in search_response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
+                return search_result["id"]["videoId"]
+    except Exception as e:
+        print(e)
