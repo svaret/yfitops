2e75e889f8f0206e77a492c7b6bb00e0738412d9
Peter Krooks <pekr@Peters-MacBook-Pro.local>
2023-01-22 14:40:40 +0100

    fixing jumpy behaviour and bad scrolling problem

--- app.py
+++ app.py
@@ -14,15 +14,19 @@ app = Flask(__name__)
 @app.route('/album-cover')
 def album_cover():
     artist = request.args.get('artist')
+
     response = requests.get(
         'https://api.discogs.com/database/search?artist=' + artist + '&type=master&format=LP',
         headers={'Authorization': ('Discogs token=%s' % DISCOGS_TOKEN)}
     )
+
     number_of_hits = len(response.json()['results'])
     if number_of_hits == 0:
-        return render_template('artist-not-found.html', artist=artist)
+        return render_template('artist-not-found.html', artist=artist, showmode=True)
+
     random_entry = response.json()['results'][randrange(number_of_hits)]
-    return render_template('index.html', artist=artist, uri=random_entry['cover_image'], title=random_entry['title'])
+
+    return render_template('index.html', artist=artist, uri=random_entry['cover_image'], title=random_entry['title'], showmode=True)
 
 
 @app.route('/youtube')
@@ -31,11 +35,13 @@ def youtube():
                            artist=(request.args.get('artist')),
                            uri=(request.args.get('imageUri')),
                            title=(request.args.get('title')),
-                           videolink=youtubeid(request.args.get('title')))
+                           videolink=youtubeid(request.args.get('title')),
+                           showmode=False)
 
 
 def youtubeid(title):
     youtube = build("youtube", "v3", developerKey=GOOGLE_API_KEY)
+
     try:
         search_response = youtube.search().list(q=title, part="id,snippet", maxResults=1).execute()
         for search_result in search_response.get("items", []):
